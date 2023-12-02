47dda94ff28f46c9ed09bca26e40fea7637762ed
goanna5 <143087684+goanna5@users.noreply.github.com>
2023-12-02 13:52:17 +1000

    day 1 updated

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # My 2023 advent of code solutions
+
+Day 1: 2 stars
--- day1.py
+++ day1.py
@@ -27,76 +27,27 @@ print(total)
 file = open("input1.txt", "r")
 all_numbers = []
 first_number = -1
+number_words = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6",
+                 "seven": "7", "eight": "8", "nine": "9"}
 
 for line in file:
     modified_line = line
 
-    i = 0
-    while i < 3:
-        # find one
-        index_one = modified_line.find('one')
-        if (index_one != -1):
-            modified_line = modified_line[:index_one+1] + "1" + modified_line[index_one+1:]
+    for i in range(3):
+        for word in number_words:
+            index = modified_line.find(word)
+            if (index != -1):
+                modified_line = modified_line[:index + 1] + number_words[word] + modified_line[index + 1:]
 
-        # find two
-        index_two = modified_line.find('two')
-        if index_two != -1:
-            modified_line = modified_line[:index_two+1] + "2" + modified_line[index_two+1:]
-
-        # find three
-        index_three = modified_line.find('three')
-        if index_three != -1:
-            modified_line = modified_line[:index_three+1] + "3" + modified_line[index_three+1:]
-
-        # find four
-        index_four = modified_line.find('four')
-        if index_four != -1:
-            modified_line = modified_line[:index_four+1] + "4" + modified_line[index_four+1:]
-
-        # find five
-        index_five = modified_line.find('five')
-        if index_five != -1:
-            modified_line = modified_line[:index_five+1] + "5" + modified_line[index_five+1:]
-
-        # find six
-        index_six = modified_line.find('six')
-        if index_six != -1:
-            modified_line = modified_line[:index_six+1] + "6" + modified_line[index_six+1:]
-
-        # find seven
-        index_seven = modified_line.find('seven')
-        if index_seven != -1:
-            modified_line = modified_line[:index_seven+1] + "7" + modified_line[index_seven+1:]
-
-        # find seven
-        index_seven = modified_line.find('seven')
-        if index_seven != -1:
-            modified_line = modified_line[:index_seven+1] + "7" + modified_line[index_seven+1:]
-
-        # find eight
-        index_eight = modified_line.find('eight')
-        if index_eight != -1:
-            modified_line = modified_line[:index_eight+1] + "8" + modified_line[index_eight+1:]
-
-        # find nine
-        index_nine = modified_line.find('nine')
-        if index_nine != -1:
-            modified_line = modified_line[:index_nine+1] + "9" + modified_line[index_nine+1:]
-        
-        i += 1
-
-    
     for letter in modified_line:
         if not letter.isalpha() and letter != '\n':
             if first_number == -1:
                 first_number = letter
             second_number = letter
     
-
     all_numbers.append(int(str(first_number)+str(second_number)))
     first_number = -1
         
-
 total = 0
 for number in all_numbers:
     total += number
